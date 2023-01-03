f6ba54d5b1ffcaa5f208fa25993a362d9fcf91db
Branda0 <g.brandao.dev@gmail.com>
2023-01-03 11:07:28 +0100

    updated pokemon module with new providers

--- pokedex-nestjs/src/pokemon/pokemon.module.ts
+++ pokedex-nestjs/src/pokemon/pokemon.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { PokemonController } from './pokemon.controller';
 import { GetPokemonsHandler } from './queries/get-pokemons/get-pokemons.handler';
-
-// import { GetPokemonsService } from './queries/get-pokemons/get-pokemons.service';
+import { PokemonApiService } from './api/pokemon.api.service';
 
 @Module({
   imports: [CqrsModule],
   controllers: [PokemonController],
-  providers: [GetPokemonsHandler],
+  providers: [GetPokemonsHandler, PokemonApiService],
 })
 export class PokemonModule {}
