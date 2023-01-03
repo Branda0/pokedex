ae6dcb270a81df7a06e7b69dc242580d99d7d626
Branda0 <g.brandao.dev@gmail.com>
2023-01-03 11:10:44 +0100

    created pokemon controller, will be in charge of listening to client http GET routes and call the appropriated query via queryBus execution

--- pokedex-nestjs/src/pokemon/pokemon.controller.ts
+++ pokedex-nestjs/src/pokemon/pokemon.controller.ts
@@ -1,19 +1,20 @@
 // pokemon.controller.ts
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Query } from '@nestjs/common';
 import { QueryBus } from '@nestjs/cqrs';
 import { GetPokemonsQuery } from './queries/get-pokemons/get-pokemons.query';
 
-@Controller('pokemons')
+@Controller('pokemon')
 export class PokemonController {
   constructor(private readonly queryBus: QueryBus) {}
 
   @Get()
-  async getPokemons() {
-    return this.queryBus.execute(new GetPokemonsQuery());
+  async getPokemons(@Query() query: GetPokemonsQuery) {
+    return this.queryBus.execute(new GetPokemonsQuery(query));
   }
 
   @Get(':id')
-  async getPokemonById(@Param('id') id: string) {
-    return this.queryBus.execute(new GetPokemonsQuery(id));
+  async getPokemonById(@Param('id', ParseIntPipe) id: number) {
+    return this.queryBus.execute(new GetPokemonsQuery({ pokemonId: id }));
+    // return this.queryBus.execute(new GetPokemonsQuery(id));
   }
 }
