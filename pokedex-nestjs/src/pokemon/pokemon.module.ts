d995df64f5b6e0888f6091d301526dcffeba039b
Branda0 <g.brandao.dev@gmail.com>
2023-01-04 10:51:16 +0100

    added GetPokemonHandler to providers

--- pokedex-nestjs/src/pokemon/pokemon.module.ts
+++ pokedex-nestjs/src/pokemon/pokemon.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { PokemonController } from './pokemon.controller';
 import { GetPokemonsHandler } from './queries/get-pokemons/get-pokemons.handler';
+import { GetPokemonHandler } from './queries/get-pokemon/get-pokemon.handler';
 import { PokemonApiService } from './api/pokemon.api.service';
 
 @Module({
   imports: [CqrsModule],
   controllers: [PokemonController],
-  providers: [GetPokemonsHandler, PokemonApiService],
+  providers: [GetPokemonsHandler, GetPokemonHandler, PokemonApiService],
 })
 export class PokemonModule {}
