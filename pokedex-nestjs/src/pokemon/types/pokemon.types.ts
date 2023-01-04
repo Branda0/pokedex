26f6eb1d3c3842d0b2a384df4e4748b9293c612d
Branda0 <g.brandao.dev@gmail.com>
2023-01-04 10:23:04 +0100

    added some types to match different state of data (coming from external api, in internal service, returned to client ...)

--- pokedex-nestjs/src/pokemon/types/pokemon.types.ts
+++ pokedex-nestjs/src/pokemon/types/pokemon.types.ts
@@ -1,4 +1,4 @@
-// All proprieties of Pokemon, as returned to client from pokemon/:id endpoint
+// proprieties of Pokemon, as returned to client from pokemon/:id endpoint
 export interface Pokemon {
   name: string;
   id: number;
@@ -6,18 +6,34 @@ export interface Pokemon {
   weight: number;
   image: string;
   types: string[];
-  description: string;
-  color: string;
 }
 
-//proporties of pokemon list item returned by the "pokeapi.co/api/v2/pokemon" endpoint
+//properties of PokemonList, as returned to client from /pokemon endpoint
+export interface PokemonList {
+  count: number;
+  data: Pokemon[];
+}
+
+export interface PokemonDetails {
+  description: string;
+  color: string;
+  evolutions: any[]; // need todefine evolution format
+}
+
+//proporties of pokemon list returned from pokemon api service getPokemonList
+export interface PokemonResultList {
+  count: number;
+  result: PokemonResultItem[];
+}
+
+//proporties of pokemon item returned by the "pokeapi.co/api/v2/pokemon" endpoint
 export interface PokemonApiItem {
   name: string;
   url: string;
 }
 
-//proporties returned to client from /pokemon endpoint
-export interface PokemonDataList {
-  count: number;
-  data: Pokemon[];
+//proporties of pokemon list item returned from pokemon api service getPokemonList
+export interface PokemonResultItem {
+  name: string;
+  id: number;
 }
