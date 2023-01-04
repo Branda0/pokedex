94ae23a05b26f9a58e8717331d5adbdab3cf9551
Branda0 <g.brandao.dev@gmail.com>
2023-01-04 12:30:06 +0100

    added svg of pokemon types icon assets

---
+++ pokedex-react/src/assets/svg-types/index.ts
@@ -0,0 +1,37 @@
+import bugLogo from "./bug.svg";
+import darkLogo from "./dark.svg";
+import dragonLogo from "./dragon.svg";
+import electricLogo from "./electric.svg";
+import fairyLogo from "./fairy.svg";
+import fightingLogo from "./fighting.svg";
+import fireLogo from "./fire.svg";
+import flyingLogo from "./flying.svg";
+import ghostLogo from "./ghost.svg";
+import grassLogo from "./grass.svg";
+import iceLogo from "./ice.svg";
+import normalLogo from "./normal.svg";
+import poisonLogo from "./poison.svg";
+import psychicLogo from "./psychic.svg";
+import rockLogo from "./rock.svg";
+import steelLogo from "./steel.svg";
+import waterLogo from "./water.svg";
+
+export {
+  bugLogo,
+  darkLogo,
+  dragonLogo,
+  electricLogo,
+  fairyLogo,
+  fightingLogo,
+  fireLogo,
+  flyingLogo,
+  ghostLogo,
+  grassLogo,
+  iceLogo,
+  normalLogo,
+  poisonLogo,
+  psychicLogo,
+  rockLogo,
+  steelLogo,
+  waterLogo,
+};
