dfd14996c081fe1a921fd54af245d891327769be
Branda0 <g.brandao.dev@gmail.com>
2023-01-04 10:45:25 +0100

    updated GetPokemons query handle to make use of different services and return desired data to client

--- pokedex-nestjs/src/pokemon/queries/get-pokemons/get-pokemons.handler.ts
+++ pokedex-nestjs/src/pokemon/queries/get-pokemons/get-pokemons.handler.ts
@@ -1,24 +1,30 @@
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
-import { GetPokemonsQuery, GetPokemonQueryProps } from './get-pokemons.query';
+import { GetPokemonsQuery } from './get-pokemons.query';
 import { PokemonApiService } from 'src/pokemon/api/pokemon.api.service';
+import {
+  PokemonList,
+  PokemonResultList,
+} from 'src/pokemon/types/pokemon.types';
 
 @QueryHandler(GetPokemonsQuery)
 export class GetPokemonsHandler implements IQueryHandler<GetPokemonsQuery> {
   constructor(private readonly pokemonApiService: PokemonApiService) {}
 
-  async execute(query: GetPokemonsQuery): Promise<any> {
-    const { pokemonId, limit, page, offset } = query;
+  async execute(query: GetPokemonsQuery): Promise<PokemonList> {
+    try {
+      const pokemonList: PokemonResultList =
+        await this.pokemonApiService.getPokemonList(query);
 
-    console.log({ query });
+      const pokemonPromises = pokemonList.result.map(async (pokemonItem) => {
+        return await this.pokemonApiService.getPokemon(pokemonItem.id);
+      });
 
-    // get one pokemon
-    if (pokemonId) {
-      const res = this.pokemonApiService.getPokemon(query);
-      return await res;
+      return {
+        count: pokemonList.count,
+        data: await Promise.all(pokemonPromises),
+      };
+    } catch (error) {
+      throw new Error(`Error fetching `);
     }
-
-    // get list of pokemons
-    const res = this.pokemonApiService.getPokemonList(query);
-    return await res;
   }
 }
