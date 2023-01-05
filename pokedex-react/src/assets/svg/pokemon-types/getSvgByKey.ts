759a53b25b66c9dbc831521639f323e6495655db
Branda0 <g.brandao.dev@gmail.com>
2023-01-05 10:06:43 +0100

    created getSvgByKey to be able to dynamically use svg file depending on pokemon type on TypeCard component

---
+++ pokedex-react/src/assets/svg/pokemon-types/getSvgByKey.ts
@@ -0,0 +1,47 @@
+import {
+  bug,
+  dark,
+  dragon,
+  electric,
+  fairy,
+  fighting,
+  fire,
+  flying,
+  ghost,
+  grass,
+  ground,
+  ice,
+  normal,
+  poison,
+  psychic,
+  rock,
+  steel,
+  water,
+} from "./";
+
+const svg = {
+  bug,
+  dark,
+  dragon,
+  electric,
+  fairy,
+  fighting,
+  fire,
+  flying,
+  ghost,
+  grass,
+  ground,
+  ice,
+  normal,
+  poison,
+  psychic,
+  rock,
+  steel,
+  water,
+};
+
+function getSvgByKey(key: string) {
+  return svg[key as keyof typeof svg];
+}
+
+export default getSvgByKey;
