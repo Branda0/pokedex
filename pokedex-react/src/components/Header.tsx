ea378640166db5f271b27e7557e73ee63c114aea
Branda0 <g.brandao.dev@gmail.com>
2023-01-05 09:43:58 +0100

    added bottom shadow

--- pokedex-react/src/components/Header.tsx
+++ pokedex-react/src/components/Header.tsx
@@ -1,7 +1,7 @@
 import headerLogo from "../assets/img/pokedexHeader.png";
 const Header = () => {
   return (
-    <header className="flex justify-center border-b-2 p-4 bg-gray-100 ">
+    <header className="flex justify-center shadow-md p-4 bg-gray-100 ">
       <img src={headerLogo} className=" h-24 object-contain" alt="Pokedex Title logo" />
     </header>
   );
