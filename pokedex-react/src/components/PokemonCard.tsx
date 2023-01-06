704ca34585a27bf6340b0fcc9c36d005174028b6
Branda0 <g.brandao.dev@gmail.com>
2023-01-06 12:09:48 +0100

    updated Pokemon Card, now providing a link to a separate pokemon specific route

--- pokedex-react/src/components/PokemonCard.tsx
+++ pokedex-react/src/components/PokemonCard.tsx
@@ -1,24 +1,33 @@
+import { Link } from "react-router-dom";
 import { IPokemon } from "../types/pokemon";
 import TypeCard from "./TypeCard";
 
 const PokemonCard = ({ pokemon }: { pokemon: IPokemon }) => {
   return (
-    <div className={`flex flex-col p-4 shadow-md rounded-lg bg-${pokemon.types[0]}Light `}>
+    <Link
+      className={`flex flex-col justify-between border-2 border-gray-700 p-4 rounded-lg bg-${pokemon.types[0]}Light duration-300 cursor-pointer hover:scale-[1.02] hover:shadow-md md:p-2 lg:p-4  `}
+      to={`/pokemon/${pokemon.id}`}
+      state={{ pokemonFromCard: pokemon }}
+    >
       <img
         src={pokemon.image}
         className=" self-center w-[70%] -translate-y-14 -mb-14 object-contain "
-        alt="Pokedex Title logo"
+        alt="Pokemon"
       />
 
-      <p className=" text-sm text-gray-700 font-semibold ">{`#${String(pokemon.id).padStart(3, "0")}`}</p>
-      <span className=" mb-2 capitalize text-4xl text-white font-semibold ">{pokemon.name}</span>
+      <div className="flex flex-col  ">
+        <p className=" text-sm text-gray-700 font-semibold ">{`#${String(pokemon.id).padStart(3, "0")}`}</p>
+        <span className=" mb-4 capitalize text-4xl text-white font-semibold sm:text-3xl lg:text-4xl ">
+          {pokemon.name}
+        </span>
 
-      <div className="flex flex-row gap-2">
-        {pokemon.types.map((type: string) => (
-          <TypeCard key={pokemon.id + type} type={type} />
-        ))}
+        <div className="flex flex-row gap-2">
+          {pokemon.types.map((type: string) => (
+            <TypeCard key={pokemon.id + type} type={type} />
+          ))}
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
