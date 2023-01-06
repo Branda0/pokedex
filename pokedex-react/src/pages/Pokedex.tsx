7923af602e4c0dcec77541869c44a35f40244c56
Branda0 <g.brandao.dev@gmail.com>
2023-01-06 12:02:14 +0100

    reworked page, fetch is now done in a separate component

--- pokedex-react/src/pages/Pokedex.tsx
+++ pokedex-react/src/pages/Pokedex.tsx
@@ -1,57 +1,14 @@
-import { Link } from "react-router-dom";
-import { useState } from "react";
-import { useQuery } from "react-query";
-import { fetchPokemons } from "../fetchs/pokemon";
-import { PokemonCard } from "../components";
-import { bug, fire } from "../assets/svg/pokemon-types";
-import { IPokemon, IPokemonList } from "../types/pokemon";
+import { useState, useEffect } from "react";
+import { Pagination, SearchBar, PokemonsGrid } from "../components";
 
 const Pokedex = () => {
-  const [page, setPage] = useState(1);
-
-  const queryKey = [`pokemons${page}`];
-
-  const handlePage = () => {
-    setPage((prev) => prev + 1);
-  };
-  console.log("RENDER");
-  const { isLoading, isError, data, error } = useQuery(queryKey, () => fetchPokemons(page), {
-    staleTime: 60000,
-  });
-
-  const pokemonListData: IPokemonList = data;
-
-  if (isLoading) {
-    return (
-      <div>
-        <h1>LOADING ...</h1>
-      </div>
-    );
-  }
-
-  if (isError) {
-    return (
-      <div>
-        <h1>{`error : ${error}`}</h1>
-      </div>
-    );
-  }
+  const [page, setPage] = useState<number>(1);
+  const [searchValue, setSearchValue] = useState<string>("");
 
   return (
-    <div>
-      <div>
-        <span>SEARCH BAR</span>
-      </div>
-      <h1>{page}</h1>
-      <h2>{`${pokemonListData.count}`}</h2>
-      <button onClick={handlePage}> Page + </button>
-      {/* <img src={bug} alt="bug" /> */}
-      <div className="grid grid-cols-3 gap-12 p-2 mt-10 ">
-        {pokemonListData.data.map((pokemon: IPokemon) => (
-          <PokemonCard key={pokemon.id} pokemon={pokemon} />
-        ))}
-      </div>
-      <Link to={`/pokemon/3`}>CLICK</Link>
+    <div className="flex flex-1 flex-col w-full max-w-5xl m-auto px-2 py-10 sm:px-6 xl:px-0">
+      <SearchBar searchValue={searchValue} setPage={setPage} setSearchValue={setSearchValue} />
+      <PokemonsGrid searchValue={searchValue} page={page} setPage={setPage} />
     </div>
   );
 };
