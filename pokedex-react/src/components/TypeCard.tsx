26eaba769df688a55191ce60c33d7e55a958c0bd
Branda0 <g.brandao.dev@gmail.com>
2023-01-06 12:04:16 +0100

    minor style update

--- pokedex-react/src/components/TypeCard.tsx
+++ pokedex-react/src/components/TypeCard.tsx
@@ -4,7 +4,7 @@ const TypeCard = ({ type }: { type: string }) => {
   return (
     <div className={`flex justify-center items-center  bg-${type}Dark rounded-full px-5 py-[0.3rem]`}>
       <img src={getSvgByKey(type)} className=" h-3 mr-1" alt={`type ${type} Logo`} />
-      <span className=" capitalize text-white text-sm font-semibold">{type}</span>
+      <span className=" capitalize text-white text-sm font-semibold md:text-xs lg:text-sm">{type}</span>
     </div>
   );
 };
