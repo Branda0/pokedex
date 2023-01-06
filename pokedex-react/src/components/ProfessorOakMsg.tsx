d696911d7d7ba3a51a0da1aa7187bef3a4b7231d
Branda0 <g.brandao.dev@gmail.com>
2023-01-06 12:05:57 +0100

    added custom message handling compoent

---
+++ pokedex-react/src/components/ProfessorOakMsg.tsx
@@ -0,0 +1,15 @@
+import professorOakImg from "../assets/img/professorOak.png";
+const ProfessorOakMsg = ({ message }: { message: string }) => {
+  return (
+    <div className="flex flex-col flex-1 justify-center items-center ">
+      <img className="w-32 mb-4 " src={professorOakImg} alt="professor Oak" />
+      <p
+        className="border-4 border-double border-gray-600
+        px-3 py-2 rounded-lg font-code max-w-md "
+      >
+        {message}
+      </p>
+    </div>
+  );
+};
+export default ProfessorOakMsg;
