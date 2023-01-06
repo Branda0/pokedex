7818c06938ed660b8a562dccc728af8077eb2020
Branda0 <g.brandao.dev@gmail.com>
2023-01-06 15:23:12 +0100

    styled page not found

--- pokedex-react/src/pages/NotFound.tsx
+++ pokedex-react/src/pages/NotFound.tsx
@@ -1,10 +1,15 @@
 import { Link } from "react-router-dom";
+import { ProfessorOakMsg } from "../components";
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>LOST</h1>
-      <Link to={`/`}>Back Home</Link>
+    <div className="flex flex-col items-center w-full max-w-5xl m-auto px-2 py-10 sm:px-6 xl:px-0">
+      <ProfessorOakMsg
+        message={"It's seems that you are lost, be careful you could face a wild Pokemon in this area ..."}
+      />
+      <Link className="flex w-fit justify-center mt-8" to={`/`}>
+        <p className=" px-6 py-2 text-sm rounded-md bg-[#346BBD] text-white">Back Home</p>
+      </Link>
     </div>
   );
 };
