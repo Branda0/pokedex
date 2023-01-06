15a2316ebd7ae7c4b0b1381b1034e983f0415570
Branda0 <g.brandao.dev@gmail.com>
2023-01-06 15:24:44 +0100

    minimal changes on header logo size on different media queries

--- pokedex-react/src/components/Header.tsx
+++ pokedex-react/src/components/Header.tsx
@@ -2,9 +2,11 @@ import headerLogo from "../assets/img/pokedexHeader.png";
 import pokeballLogo from "../assets/img/pokeball.png";
 const Header = () => {
   return (
-    <header className="flex shadow-md h-28 p-4 bg-gray-100 ">
+    <header className="flex shadow-md h-20 p-3 bg-gray-100 sm:h-32 sm:p-6">
       <div className="justify-center flex w-full ">
+        {/* <img src={pokeballLogo} className="flex  self-center object-contain h-6" alt="Pokeball" /> */}
         <img src={headerLogo} className="object-contain mx-4" alt="Pokedex Title" />
+        {/* <img src={pokeballLogo} className="flex self-center object-contain h-6" alt="Pokeball" /> */}
       </div>
     </header>
   );
