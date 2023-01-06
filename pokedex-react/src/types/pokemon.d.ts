4b87343fbcb5fe5236a2d256a667ce872e5444db
Branda0 <g.brandao.dev@gmail.com>
2023-01-06 11:57:38 +0100

    added pokemon details type interface

--- pokedex-react/src/types/pokemon.d.ts
+++ pokedex-react/src/types/pokemon.d.ts
@@ -1,13 +1,21 @@
 export interface IPokemon {
   name: string;
-  id: number;
+  id: string;
   height: number;
   weight: number;
   image: string;
   types: string[];
+  abilities: string[];
 }
 
 export interface IPokemonList {
   count: number;
   data: Pokemon[];
+}
+
+export interface IPokemonDetails {
+  description: string;
+  habitat: string;
+  shape: string;
+  evolutions: any[]; // need todefine evolution format
 }
