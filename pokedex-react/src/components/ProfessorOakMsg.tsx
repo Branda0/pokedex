3b853fed25ab1042fdb764efeeca30e47835e6d1
Branda0 <g.brandao.dev@gmail.com>
2023-01-06 15:38:07 +0100

    added padding to Professor Oak message component

--- pokedex-react/src/components/ProfessorOakMsg.tsx
+++ pokedex-react/src/components/ProfessorOakMsg.tsx
@@ -1,7 +1,7 @@
 import professorOakImg from "../assets/img/professorOak.png";
 const ProfessorOakMsg = ({ message }: { message: string }) => {
   return (
-    <div className="flex flex-col flex-1 justify-center items-center ">
+    <div className="flex flex-col flex-1 justify-center items-center px-4">
       <img className="w-32 mb-4 " src={professorOakImg} alt="professor Oak" />
       <p
         className="border-4 border-double border-gray-600
