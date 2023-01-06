2abd8e77ef1258d9def12c744888255dfbebae67
Branda0 <g.brandao.dev@gmail.com>
2023-01-06 16:55:58 +0100

    added debounce hook to delay data fetching on searchbar text update

--- pokedex-react/src/hooks/useDebounce.ts
+++ pokedex-react/src/hooks/useDebounce.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 
+// hook to delay fetch 500ms after last search bar update
 const useDebounce = (value: any, delay: number) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
