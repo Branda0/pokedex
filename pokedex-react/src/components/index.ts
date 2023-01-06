07df9ff4a33717528386e75dd912481584cb90ab
Branda0 <g.brandao.dev@gmail.com>
2023-01-06 12:34:02 +0100

    updated component import index

--- pokedex-react/src/components/index.ts
+++ pokedex-react/src/components/index.ts
@@ -2,5 +2,28 @@ import Header from "./Header";
 import Footer from "./Footer";
 import PokemonCard from "./PokemonCard";
 import TypeCard from "./TypeCard";
+import Pagination from "./Pagination";
+import PaginationIcon from "./PaginationIcon";
+import SearchBar from "./SearchBar";
+import PokemonsGrid from "./PokemonsGrid";
+import Loading from "./Loading";
+import ProfessorOakMsg from "./ProfessorOakMsg";
+import PokemonHero from "./PokemonHero";
+import PokemonDetails from "./PokemonDetails";
+import DetailsStats from "./DetailStats";
 
-export { Header, Footer, PokemonCard, TypeCard };
+export {
+  Header,
+  Footer,
+  PokemonCard,
+  TypeCard,
+  Pagination,
+  PaginationIcon,
+  PokemonsGrid,
+  Loading,
+  SearchBar,
+  ProfessorOakMsg,
+  PokemonHero,
+  PokemonDetails,
+  DetailsStats,
+};
