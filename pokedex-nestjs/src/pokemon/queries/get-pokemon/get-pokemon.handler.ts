7e1f1d7ad95745510ff103c84279897ee1fce5c2
Branda0 <g.brandao.dev@gmail.com>
2023-01-06 12:35:59 +0100

    added out of scope pokemon id erro handlinge

--- pokedex-nestjs/src/pokemon/queries/get-pokemon/get-pokemon.handler.ts
+++ pokedex-nestjs/src/pokemon/queries/get-pokemon/get-pokemon.handler.ts
@@ -8,9 +8,16 @@ export class GetPokemonHandler implements IQueryHandler<GetPokemonQuery> {
   constructor(private readonly pokemonApiService: PokemonApiService) {}
 
   async execute(query: GetPokemonQuery): Promise<Pokemon> {
+    const MAX_POKEMON = 905;
     try {
-      return await this.pokemonApiService.getPokemon(query.pokemonId);
+      if (query.pokemonId > MAX_POKEMON || query.pokemonId < 1) {
+        throw new Error('400');
+      } else {
+        return await this.pokemonApiService.getPokemon(query.pokemonId);
+      }
     } catch (error) {
+      if (error.message === '400') throw new Error('400');
+
       throw new Error(
         `Error fetching Pokemon with id ${query.pokemonId}: ${error.message}`,
       );
