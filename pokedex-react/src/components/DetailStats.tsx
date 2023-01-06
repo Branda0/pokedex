9945039ec77e5c8776b8614a588deefda86ed877
Branda0 <g.brandao.dev@gmail.com>
2023-01-06 18:55:51 +0100

    update to not display abilities if abilities array is empty

--- pokedex-react/src/components/DetailStats.tsx
+++ pokedex-react/src/components/DetailStats.tsx
@@ -19,18 +19,20 @@ const DetailsStats = ({
           <span className={`capitalize decoration-${pokemon.types[0]}Dark`}>{pokemon.weight}</span>
         </div>
       </div>
-      <div className="flex py-1 border-b-2 border-gray-200">
-        <div className="flex flex-row items-center  ">
-          <span className="flex w-20 capitalize text-gray-500">abilities</span>
-          <div className="flex flex-wrap">
-            {pokemon.abilities.map((ability, index) => (
-              <span key={index} className="capitalize leading-10 line mr-4">
-                {ability}
-              </span>
-            ))}
+      {pokemon?.abilities.length ? (
+        <div className="flex py-1 border-b-2 border-gray-200">
+          <div className="flex flex-row items-center  ">
+            <span className="flex w-20 capitalize text-gray-500">abilities</span>
+            <div className="flex flex-wrap">
+              {pokemon?.abilities.map((ability, index) => (
+                <span key={index} className="capitalize leading-10 line mr-4">
+                  {ability}
+                </span>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
+      ) : null}
       {pokemonDetails?.habitat ? (
         <div className="flex gap-10 py-4 border-b-2 border-gray-200">
           <div className="flex flex-row items-center  ">
