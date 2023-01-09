becc98aeac494c5b716887acbd98307e75011b3e
Branda0 <g.brandao.dev@gmail.com>
2023-01-09 13:12:36 +0100

    scrolling to top of the screen when using page navigation

--- pokedex-react/src/pages/Pokedex.tsx
+++ pokedex-react/src/pages/Pokedex.tsx
@@ -1,9 +1,13 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { SearchBar, PokemonsGrid } from "../components";
 
 const Pokedex = () => {
   const [page, setPage] = useState<number>(Number(sessionStorage.getItem("current-page")) || 1);
   const [searchValue, setSearchValue] = useState<string>(sessionStorage.getItem("search-value") || "");
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  }, [page]);
 
   return (
     <div className="flex flex-1 flex-col w-full max-w-5xl m-auto px-2 sm:mb-10 sm:px-6 xl:px-0">
