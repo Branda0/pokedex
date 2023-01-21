e1e736f774ea0e2476fef1784cf18608aefbb417
Branda0 <g.brandao.dev@gmail.com>
2023-01-21 18:08:47 +0100

    added various dtos for create Trainer logic

---
+++ pokedex-nestjs/src/trainer/commands/create-trainer/create-trainer.dtos.ts
@@ -0,0 +1,28 @@
+import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class CreateTrainerRequestDto {
+  @MaxLength(50)
+  @MinLength(5)
+  @IsEmail()
+  readonly email: string;
+
+  @MaxLength(50)
+  @MinLength(4)
+  @IsString()
+  readonly userName: string;
+
+  @MaxLength(100)
+  @MinLength(5)
+  @IsString()
+  password: string;
+}
+
+export class CreateTrainerHashedRequestDto {
+  readonly email: string;
+  readonly userName: string;
+  readonly hash: string;
+}
+
+export class CreateTrainerResponseDto {
+  readonly id: string;
+}
