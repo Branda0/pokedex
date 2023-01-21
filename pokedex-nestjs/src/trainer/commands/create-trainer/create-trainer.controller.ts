e50b2e85d0b9a7cd139775ac9d5df7df554d75d4
Branda0 <g.brandao.dev@gmail.com>
2023-01-21 23:34:31 +0100

    handling duplicate trainer db creation error

--- pokedex-nestjs/src/trainer/commands/create-trainer/create-trainer.controller.ts
+++ pokedex-nestjs/src/trainer/commands/create-trainer/create-trainer.controller.ts
@@ -1,19 +1,14 @@
 // pokemon.controller.ts
-import {
-  Controller,
-  HttpException,
-  HttpStatus,
-  Post,
-  Body,
-} from '@nestjs/common';
+import { Controller, ConflictException, Post, Body } from '@nestjs/common';
 import { hash } from 'argon2';
 import { CommandBus } from '@nestjs/cqrs';
 import {
   CreateTrainerHashedRequestDto,
   CreateTrainerRequestDto,
+  CreateTrainerResponseDto,
 } from './create-trainer.dtos';
-import { CreateTrainerResponseDto } from './create-trainer.response.dto';
 import { CreateTrainerCommand } from './create-trainer.command';
+import { Result } from 'src/shared/result.type';
 
 @Controller('trainer')
 export class CreateTrainerController {
@@ -23,15 +18,17 @@ export class CreateTrainerController {
   async createTrainer(
     @Body() dto: CreateTrainerRequestDto,
   ): Promise<CreateTrainerResponseDto> {
-    try {
-      const command: CreateTrainerHashedRequestDto = {
-        email: dto.email,
-        userName: dto.userName,
-        hash: await hash(dto.password),
-      };
-      return this.commandBus.execute(new CreateTrainerCommand(command));
-    } catch (error) {
-      throw new HttpException(error.message, HttpStatus.NOT_FOUND);
-    }
+    const command: CreateTrainerHashedRequestDto = {
+      email: dto.email,
+      userName: dto.userName,
+      hash: await hash(dto.password),
+    };
+
+    const result: Result<CreateTrainerResponseDto, string> =
+      await this.commandBus.execute(new CreateTrainerCommand(command));
+
+    if (result.status === 'err') throw new ConflictException(result.data);
+
+    return result.data;
   }
 }
