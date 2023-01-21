28b4b430801ceb53a53efb9437a2244492316ba6
Branda0 <g.brandao.dev@gmail.com>
2023-01-21 23:33:23 +0100

    added createTrainer handler to manage the route create/route logic

---
+++ pokedex-nestjs/src/trainer/commands/create-trainer/create-trainer.handler.ts
@@ -0,0 +1,32 @@
+import { ICommandHandler, CommandHandler } from '@nestjs/cqrs';
+import { Result } from 'src/shared/result.type';
+import { TrainerEntity } from 'src/trainer/domain/trainer.entity';
+import { TrainerRepository } from 'src/trainer/domain/trainer.repository';
+import { CreateTrainerCommand } from './create-trainer.command';
+import { CreateTrainerResponseDto } from './create-trainer.dtos';
+
+@CommandHandler(CreateTrainerCommand)
+export class CreateTrainerHandler
+  implements ICommandHandler<CreateTrainerCommand>
+{
+  constructor(private readonly repository: TrainerRepository) {}
+
+  async execute(
+    command: CreateTrainerCommand,
+  ): Promise<Result<CreateTrainerResponseDto, string>> {
+    const trainerEntity = TrainerEntity.create(command);
+    console.log('created trainer :', trainerEntity);
+
+    const trainerRecord = await this.repository.findOneByEmail(command.email);
+
+    if (trainerRecord)
+      return { status: 'err', data: 'Trainer credentials already exists' };
+
+    const newTrainer = await this.repository.insert(
+      trainerEntity,
+      command.hashPassword,
+    );
+
+    return { status: 'ok', data: { id: newTrainer.id } };
+  }
+}
