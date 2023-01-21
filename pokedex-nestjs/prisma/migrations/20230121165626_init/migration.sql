ba26fbd90f9b54246b469ae4910afecc51789716
Branda0 <g.brandao.dev@gmail.com>
2023-01-21 18:03:43 +0100

    configured docker with prisma database and made first  migration

---
+++ pokedex-nestjs/prisma/migrations/20230121165626_init/migration.sql
@@ -0,0 +1,28 @@
+-- CreateTable
+CREATE TABLE "Trainer" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "email" TEXT NOT NULL,
+    "userName" TEXT NOT NULL,
+    "hash" TEXT NOT NULL,
+    "catchedPokemons" JSONB[],
+
+    CONSTRAINT "Trainer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Pokemon" (
+    "id" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "name" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "types" TEXT[],
+
+    CONSTRAINT "Pokemon_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Trainer_email_key" ON "Trainer"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Pokemon_name_key" ON "Pokemon"("name");
