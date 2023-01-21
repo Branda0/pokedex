4eb884e1fc7ccec353162c003a064f2d9f822ed7
Branda0 <g.brandao.dev@gmail.com>
2023-01-21 23:31:07 +0100

    added TrainerRepository to trainerModule providers

--- pokedex-nestjs/src/trainer/trainer.module.ts
+++ pokedex-nestjs/src/trainer/trainer.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { CreateTrainerController } from './commands/create-trainer/create-trainer.controller';
 import { CreateTrainerHandler } from './commands/create-trainer/create-trainer.handler';
+import { TrainerRepository } from './domain/trainer.repository';
 import { TrainerMapper } from './trainer.mapper';
 
 @Module({
   imports: [CqrsModule],
   controllers: [CreateTrainerController],
-  providers: [CreateTrainerHandler, TrainerMapper],
+  providers: [CreateTrainerHandler, TrainerMapper, TrainerRepository],
 })
 export class TrainerModule {}
