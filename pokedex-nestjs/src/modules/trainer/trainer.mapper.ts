1f57e4e5c586a49488c1420e3a509fe4a306c508
Branda0 <g.brandao.dev@gmail.com>
2023-01-23 11:44:32 +0100

    modified trainer mapper to accomodate changes on de prisma db model

--- pokedex-nestjs/src/modules/trainer/trainer.mapper.ts
+++ pokedex-nestjs/src/modules/trainer/trainer.mapper.ts
@@ -1,15 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { instanceToPlain, plainToInstance } from 'class-transformer';
-
 import { Trainer as TrainerModel } from '@prisma/client';
 import { TrainerEntity } from './domain/trainer.entity';
-import { PokemonIdentity } from 'src/shared/pokemonIdentity.value-object';
 
 @Injectable()
 export class TrainerMapper {
   toPersistence(trainer: TrainerEntity, hashPassword: string): TrainerModel {
-    // const copy = trainer.getPropsCopy();
-    const copy = instanceToPlain(trainer);
+    const copy = trainer.getPropsCopy();
+
     return {
       id: copy.id,
       createdAt: copy.createdAt,
@@ -21,14 +18,10 @@ export class TrainerMapper {
   }
 
   toDomain(record: TrainerModel): TrainerEntity {
-    const catchedPokemons = plainToInstance(
-      PokemonIdentity,
-      record.catchedPokemons,
-    );
     const entity = new TrainerEntity(
       record.id,
       record.createdAt,
-      catchedPokemons,
+      record.catchedPokemons,
       {
         email: record.email,
         userName: record.userName,
